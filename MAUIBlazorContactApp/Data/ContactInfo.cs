474cc2148735ed80c7b74fd74359d9381d0c134e
VMekh8 <mekhvitaliy88@gmail.com>
2024-03-30 15:39:46 +0200

    AddUpdateContactInfo realization
    make a button for send data into database.  Write a logic for button

--- MAUIBlazorContactApp/Data/ContactInfo.cs
+++ MAUIBlazorContactApp/Data/ContactInfo.cs
@@ -1,9 +1,4 @@
 ﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MAUIBlazorContactApp.Data
 {
@@ -12,9 +7,8 @@ namespace MAUIBlazorContactApp.Data
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string? Surname { get; set; }
+        public string Surname { get; set; }
         public string PhoneNumber { get; set; }
-        public string? Email { get; set; }
-
+        public string Email { get; set; }
     }
 }
