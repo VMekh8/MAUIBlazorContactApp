000c18835de33b88ce4e4cc98a389fa3c5047247
VMekh8 <mekhvitaliy88@gmail.com>
2024-03-30 14:44:01 +0200

    add a add update page for contact

--- MAUIBlazorContactApp/Data/ContactInfo.cs
+++ MAUIBlazorContactApp/Data/ContactInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,12 @@ namespace MAUIBlazorContactApp.Data
 {
     public class ContactInfo
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
+        public string? Surname { get; set; }
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
     }
 }
